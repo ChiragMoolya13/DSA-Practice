cb0909575705d2f6d2ee8ab095edfd8824f880a0
Chirag Moolya <chiragmoolya13@gmail.com>
2025-08-08 15:51:36 +0530

    print Armstrong number from 1 to 1000

--- Week2/File6.java
+++ Week2/File6.java
@@ -7,10 +7,50 @@ Example
 
 import java.util.Scanner;
 public class File6 {
-    
+
+    public static int powerOfN(int a, int b){
+        int answer = 1;
+        for(int i = 1; i<=b;i++){
+            answer = answer * a;
+        }
+        return answer;
+    }
+    public static boolean isArmstrong(int n){
+        int copyOfN = n;
+        int numberOfDigits = 0;
+        while(n>0){
+            numberOfDigits++;
+            n=n/10;
+        }
+        n = copyOfN;
+        int sum = 0;
+
+        while(n>0){
+            int lastDigit = n%10;
+            sum = sum + powerOfN(lastDigit, numberOfDigits);
+            n=n/10;
+        }
+        if(sum == copyOfN){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        System.out.println("Enter First Number: ");
+        int startNumber = sc.nextInt();
+        System.out.println("Enter Last Number: ");
+        int lastNumber = sc.nextInt();
+
+        for(int i = startNumber; i <= lastNumber; i++ ){
+                if(isArmstrong(i)==true){
+                    System.out.println(i + " ");
+                }
+        }
         
     }
 }
